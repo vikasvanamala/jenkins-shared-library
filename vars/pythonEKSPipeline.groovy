ca641842fb3d3de242673d2d29e92e2db90b4fcf
vikas-vanamala <vikasvanamala008@gmail.com>
2026-06-02 18:45:55 +0530

    jenkins

---
+++ vars/pythonEKSPipeline.groovy
@@ -0,0 +1,163 @@
+// call is the default function name
+def call (Map configMap){
+    pipeline {
+        agent {
+            node {
+                label 'AGENT-1'
+            }
+        }
+        environment {
+            COURSE = "jenkins"
+            appVersion = ""
+            ACC_ID = "215446237872"
+            PROJECT = configMap.get("project")
+            COMPONENT = configMap.get("component")
+        }
+        options {
+            timeout(time: 30, unit: 'MINUTES') 
+            disableConcurrentBuilds()
+        }
+        stages {
+            stage('Read Version') {
+                steps {
+                    script {
+                        appVersion = readFile(file: 'version')
+                        echo "app version: ${appVersion}"
+                    }
+                }
+            }
+            stage('Installing Dependecies') {
+                steps {
+                    script {
+                        sh """
+                            pip3 install -r requirements.txt
+                        """    
+                    }  
+                }
+            }
+            stage('Unit Test') {
+                steps {
+                    script{
+                        sh """
+                            echo test
+                        """
+                    }
+                }
+            }
+            //Here you need to select scanner tool and send the analysis to server
+            // stage('Sonar Scan'){
+            //     environment {
+            //         def scannerHome = tool 'sonar-8.0'
+            //     }
+            //     steps {
+            //         script{
+            //             withSonarQubeEnv('sonar-server') {
+            //                 sh  "${scannerHome}/bin/sonar-scanner"
+            //             }
+            //         }
+            //     }
+            // }
+            // stage('Quality Gate') {
+            //     steps {
+            //         timeout(time: 1, unit: 'HOURS') {
+            //             // Wait for the quality gate status
+            //             // abortPipeline: true will fail the Jenkins job if the quality gate is 'FAILED'
+            //             waitForQualityGate abortPipeline: true 
+            //         }
+            //     }
+            // }
+            stage('Dependabot Security Gate') {
+                environment {
+                    GITHUB_OWNER = 'vikasvanamala'
+                    GITHUB_REPO  = 'catalogue'
+                    GITHUB_API   = 'https://api.github.com'
+                    GITHUB_TOKEN = credentials('GITHUB_TOKEN')
+                }
+
+                steps {
+                    script{
+                        /* Use sh """ when you want to use Groovy variables inside the shell.
+                        Use sh ''' when you want the script to be treated as pure shell. */
+                        sh '''
+                        echo "Fetching Dependabot alerts..."
+
+                        response=$(curl -s \
+                            -H "Authorization: token ${GITHUB_TOKEN}" \
+                            -H "Accept: application/vnd.github+json" \
+                            "${GITHUB_API}/repos/${GITHUB_OWNER}/${GITHUB_REPO}/dependabot/alerts?per_page=100")
+
+                        echo "${response}" > dependabot_alerts.json
+
+                        high_critical_open_count=$(echo "${response}" | jq '[.[] 
+                            | select(
+                                .state == "open"
+                                and (.security_advisory.severity == "high"
+                                    or .security_advisory.severity == "critical")
+                            )
+                        ] | length')
+
+                        echo "Open HIGH/CRITICAL Dependabot alerts: ${high_critical_open_count}"
+
+                        if [ "${high_critical_open_count}" -gt 0 ]; then
+                            echo "❌ Blocking pipeline due to OPEN HIGH/CRITICAL Dependabot alerts"
+                            echo "Affected dependencies:"
+                            echo "$response" | jq '.[] 
+                            | select(.state=="open" 
+                            and (.security_advisory.severity=="high" 
+                            or .security_advisory.severity=="critical"))
+                            | {dependency: .dependency.package.name, severity: .security_advisory.severity, advisory: .security_advisory.summary}'
+                            exit 1
+                        else
+                            echo "✅ No OPEN HIGH/CRITICAL Dependabot alerts found"
+                        fi
+                        '''
+                        
+                    }
+                }
+            }
+            stage('Build Image') {
+                steps {
+                    script {
+                        withAWS(credentials: 'aws-creds', region: "us-east-1"){
+                            sh """
+                                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                docker images
+                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                            """
+                        }    
+                    }    
+                }
+            }
+        
+            stage('Trigger DEV Deploy') {
+                steps {
+                    script {
+                        build job: "../${COMPONENT}-deploy",
+                            wait: false, // Wait for completion
+                            propagate: false, // Propagate status
+                            parameters: [
+                                string(name: "appVersion", value: "${appVersion}"),
+                                string(name: "deploy_to", value: "dev")
+                            ]
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                echo 'i will always say hello/....'
+                cleanWs()
+            }
+            success {
+                echo 'I will run if success'
+            }
+            failure {
+                echo 'I will run if failure'
+            }
+            aborted {
+                echo 'pipeline is aborted'
+            }
+        }
+    }
+}
